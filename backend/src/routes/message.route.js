9822360001c83d75a51e7a7f75d8ab261cd603b6
Vineetha <vineetha.vinny312@gmail.com>
2025-05-12 13:52:40 +0530

    changes updated-3

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,7 +5,8 @@ import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/mes
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:receiverId", protectRoute, getMessages); // Changed from :id to :receiverId
-router.post("/send/:receiverId", protectRoute, sendMessage); // Changed from :id to :receiverId
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
